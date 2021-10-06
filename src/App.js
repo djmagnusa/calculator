e63e9c48264235708830364abf5965c838ffce54
Pratush Bhandari <pratush.bh@gmail.com>
2021-10-06 22:27:27 +0530

    Create structure and states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css";
 
-function App() {
+
+
+const App = () => {
+
+    const [preState, setPreState] = useState("");
+    const [curState, setCurState] = useState("");
+    const [input, setInput] = useState("0");
+    const [operator, setOperator] = useState(null);
+    const [total, setTotal] = useState(false);
+
+    const inputNum = e => {
+
+    }
+
+    const operatorType = e => {
+
+    }
+
+    const equals = e => {
+
+    }
+
+    const minusPlus = () => {
+      
+    }
+
+    const percent = () => {
+
+    }
+
+    const reset = () => {
+      
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="sreen">
+        <div classNmae="btn light-gray" onClick={reset}>AC</div>
+        <div classNmae="btn light-gray" onClick={percent}>%</div>
+        <div classNmae="btn light-gray" onClick={minusPlus}>+/-</div>
+        <div classNmae="btn orange" onClick={operatorType}>/</div>
+        <div classNmae="btn" onClick={inputNum}>7</div>
+        <div classNmae="btn" onClick={inputNum}>8</div>
+        <div classNmae="btn" onClick={inputNum}>9</div>
+        <div classNmae="btn orange" onClick={operatorType}>X</div>
+        <div classNmae="btn" onClick={inputNum}>4</div>
+        <div classNmae="btn" onClick={inputNum}>5</div>
+        <div classNmae="btn" onClick={inputNum}>6</div>
+        <div classNmae="btn orange" onClick={operatorType}>+</div>
+        <div classNmae="btn" onClick={inputNum}>1</div>
+        <div classNmae="btn" onClick={inputNum}>2</div>
+        <div classNmae="btn" onClick={inputNum}>3</div>
+        <div classNmae="btn orange" onClick={operatorType}>-</div>
+        <div classNmae="btn" onClick={inputNum}>0</div>
+        <div classNmae="btn" onClick={inputNum}>.</div>
+        <div classNmae="btn" onClick={equals}>=</div>
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
